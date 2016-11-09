078f6fce2691aea684013f08bced1da912086611
DimiturMarinov <dim.marinov@abv.bg>
2016-11-10 01:46:34 +0200

    Add files via upload

---
+++ zad 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int num, sum = 0;
+	cin >> num;
+	for (int i = num; i >= 1; i--){
+		sum = sum + i;
+	}
+	cout << sum << endl;
+
+
+
+	return 0;
+}
